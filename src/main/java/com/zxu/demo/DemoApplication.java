0bac6ef2720cb5801f6d9f037612bb187fb2a022
Thanos <15620606954@139.com>
2019-03-18 11:04:35 +0800

    添加logback日志

--- src/main/java/com/zxu/demo/DemoApplication.java
+++ src/main/java/com/zxu/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.zxu.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class DemoApplication {
 
+	Logger log = LoggerFactory.getLogger(DemoApplication.class);
+
 	@RequestMapping("/")
 	public String index(){
+		log.warn("we ==++");
 		return "Welcome";
 	}
 
