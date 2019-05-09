8279c314d84ecc61386f27a1e934a82dd2fa245c
Thanos <15620606954@139.com>
2019-05-09 16:35:44 +0800

    常规更新

---
+++ src/test/java/com/zxu/demo/LoginTest.java
@@ -0,0 +1,36 @@
+package com.zxu.demo;
+
+import junit.framework.Assert;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.junit.Test;
+
+public class LoginTest {
+    @Test
+    public void testH() {
+        // 获取SecurityManager工厂（此书使用ini配置文件初始化SecurityManager)
+        Factory<org.apache.shiro.mgt.SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+        // 获取Security实例 并绑定给SecurityUtils ，全局设置，设置一次即可
+        SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+        // 获取Subject，会自动绑定到当前线程，如果是web环境，请求结束后要解除绑定；
+        Subject subject = SecurityUtils.getSubject();
+        // 获取身份验证Token
+        UsernamePasswordToken userToken = new UsernamePasswordToken("zhang", "123");
+        // 登陆验证
+        try {
+            subject.login(userToken);
+        } catch (AuthenticationException e) {
+            e.printStackTrace();
+        }
+        // 断言用户已经登陆
+        Assert.assertEquals(true, subject.isAuthenticated());
+        // 退出
+        subject.logout();
+    }
+}
