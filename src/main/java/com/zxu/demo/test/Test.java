1ef0e0d5cce4f2df143a042e6d7aebc73bd4fffc
WraithBorg <borg8472@qq.com>
2019-11-25 15:22:53 +0800

    添加并发锁

--- src/main/java/com/zxu/demo/test/Test.java
+++ src/main/java/com/zxu/demo/test/Test.java
@@ -1,13 +1,19 @@
 package com.zxu.demo.test;
 
-import java.util.ServiceLoader;
+import com.zxu.demo.java8.jwt.JwtUtil;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Test {
     public static void main(String[] args) {
-        ServiceLoader<DogService> loaders = ServiceLoader.load(DogService.class);
-        for (DogService d : loaders) {
-            d.sleep();
-        }
+
+        Map map = new HashMap();
+        map.put("id", "a11111");
+        map.put("name", "张");
+
+        String token = JwtUtil.sign(map, 60L * 1000L * 30L);
+        System.out.println(token);
 
     }
 }
